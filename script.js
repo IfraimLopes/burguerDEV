284a4686e535a157e5a796aed781abd1e4de46e9
Ifraim Lopes <ifraimlopes213@gmail.com>
2025-12-29 15:33:53 +0000

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function showsibar(){
+    const slidebar = document.querySelector('.slidebar');
+    slidebar.style.display = 'flex';
+}
+
+
+function closesibar(){
+    const slidebar = document.querySelector('.slidebar');
+    slidebar.style.display = 'none';
+}
